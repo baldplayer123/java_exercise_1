d873132adab8cfd988874b85287232ec652f8a6c
yanis <yanis.bouseksou@epita.fr>
2022-11-17 11:30:28 +0100

    1

---
+++ src/Fibo.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+abstract class Fibo implements Command{
+    public String name () {return "fibo";}
+    public Boolean run(Scanner scanner){
+        
+        System.out.println("Entrez un nombre : ");
+        int n = Integer.parseInt(scanner.nextLine());
+        if(n <= 1) {
+            System.out.println("Le résultat est "+n);
+            return false;
+        }
+        int fib = 1;
+        int prevFib = 1;
+
+        for(int i=2; i<n; i++) {
+            int temp = fib;
+            fib+= prevFib;
+            prevFib = temp;
+        }
+        System.out.println("Le résultat est "+fib);
+        return false;
+        
+    }
+}
---
+++ src/Launcher.java
@@ -0,0 +1,31 @@
+import java.nio.file.Paths;
+import java.util.*;
+public class Launcher {
+    public static void main(String[] args){
+            List<Command> command = new ArrayList<>();
+            command.add(new Quit(){});
+            command.add(new Fibo(){});
+            command.add(new Freq(){});
+            command.add(new Predict(){});
+
+            System.out.println("Bienvenue ");
+            Scanner sc = new Scanner(System.in);
+            while (true) {
+                String inp = sc.nextLine();
+                int u = 0;
+                for (Command value : command) {
+                    if (value.name().equals(inp)) {
+                        if (value.run(sc)) {
+                            return;
+                        }else {
+                            u++;
+                        }
+                    }
+                }
+                if (u==0){
+                    System.out.println("Unknown command");
+                }
+            }
+
+    }
+}
